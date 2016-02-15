b0e674fcd02d26d2c1243de562d975962024a867
brandonfujii <brandonfujii2018@u.northwestern.edu>
2016-02-15 17:11:38 -0600

    fixed button positioning

--- public/js/script.js
+++ public/js/script.js
@@ -1,36 +1,139 @@
-var $clearButton = $(".clear-button");
-var $display = $(".display ul li");
-var $numButton = $(".num");
-var $signButton = $(".sign-button");
-var numVal = 0;
-var isCleared = false;
+var $clearButton = $(".clear-button"),
+$display = $(".display ul li"),
+$numButton = $(".num"),
+$signButton = $(".sign-button"),
+$decButton = $(".decimal-button"),
+$operation = $(".operation"),
+numVal = 0,
+nextVal = 0,
+isCleared = true,
+isDecimal = false,
+isAdd = false,
+isSub = false,
+isMult = false,
+isDiv = false,
+next = false,
+gaveRes = false;
 
 $clearButton.on("click", function() {
-  $display.empty();
-  $display.text("0");
-  numVal = 0;
-
-  isCleared = true;
+  clearDisplay();
 });
 
 $numButton.on("click", function() {
-
-  if (numVal.toString().length > 10) {
-    $display.text("Please enter in a lesser value");
-  } else {
+  if (numVal.toString().length < 15) {
     if (isCleared) {
       $display.empty();
       isCleared = false;
     }
-    var num = $(this).text();
-    var currVal = $display.text() + num;
-    $display.text(currVal);
-    numVal = parseInt($display.text());
-    console.log(numVal);
+    if (next) {
+      if (!nextVal) {
+        $display.empty();
+      }
+      var num = $(this).text();
+      var currVal = $display.text() + num;
+      $display.text(currVal);
+      nextVal = parseInt($display.text());
+    }
+    else {
+      if (gaveRes) {
+        $display.empty();
+        gaveRes = false;
+      }
+      var num = $(this).text();
+      var currVal = $display.text() + num;
+      $display.text(currVal);
+      numVal = parseInt($display.text());
+    }
   }
-
 });
 
 $signButton.on("click", function() {
-
+  numVal = -numVal;
+  $display.text(numVal);
+  console.log(numVal);
 });
+
+$decButton.on("click", function() {
+  isDecimal = true;
+});
+
+$operation.on("click", function() {
+  if (numVal) {
+    next = true;
+    console.log("next turned true");
+  }
+  switch($(this).attr("id")) {
+    case "add":
+      isAdd = true;
+      break;
+    case "subtract":
+      isSub = true;
+      break;
+    case "multiply":
+      isMult = true;
+      break;
+    case "divide":
+      isDiv = true;
+      break;
+    default:
+      if (isAdd) {
+        addNums();
+        isAdd = false;
+      }
+      else if (isSub) {
+        subNums();
+        isSub = false;
+      }
+      else if (isMult) {
+        multNums();
+        isMult = false;
+      }
+      else {
+        divNums();
+        isDiv = false;
+      }
+
+      next = false;
+  }
+});
+
+function addNums() {
+  numVal += nextVal;
+  displayResult();
+}
+
+function subNums() {
+  numVal -= nextVal;
+  displayResult();
+}
+
+function multNums() {
+  numVal *= nextVal;
+  displayResult();
+}
+
+function divNums() {
+  if (nextVal) {
+    numVal /= nextVal;
+  }
+  else {
+    numVal = NaN;
+  }
+  displayResult();
+}
+
+function displayResult() {
+  $display.text(numVal);
+  nextVal = 0;
+  gaveRes = true;
+}
+
+function clearDisplay() {
+  $display.empty();
+  $display.text("0");
+  numVal = 0;
+  isCleared = true;
+  next = false;
+  gaveRes=false;
+  console.log("next turned false");
+}
